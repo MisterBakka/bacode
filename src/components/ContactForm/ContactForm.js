2a63f78c06d425ec2b21711b4e27da432dc3b1c3
MisterBakka <130415011+MisterBakka@users.noreply.github.com>
2023-10-16 16:12:06 +0200

    formualire

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,62 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import "./ContactForm.css";
 
 function ContactForm() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const [messageStatus, setMessageStatus] = useState(null);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (name && email && message) {
-      setMessageStatus("success");
-      // Vous pouvez également envoyer les données à votre serveur ici
-    } else {
-      setMessageStatus("error");
-    }
-  };
-
   return (
     <div id="contact" className="contact-form-container">
-      <form onSubmit={handleSubmit} className="contact-form">
+      <form
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        className="contact-form"
+      >
         <h2>Contactez-moi</h2>
-        {messageStatus === "success" && (
-          <div className="message success">
-            Votre message a été envoyé avec succès !
-          </div>
-        )}
-        {messageStatus === "error" && (
-          <div className="message error">
-            Veuillez remplir tous les champs du formulaire.
-          </div>
-        )}
+        <div className="message" data-netlify="true"></div>
         <div className="form-group">
           <label htmlFor="name">Nom</label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
+          <input type="text" id="name" name="name" /> {/* Ajout de l'attribut "name" */}
         </div>
         <div className="form-group">
           <label htmlFor="email">E-mail</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+          <input type="email" id="email" name="email" /> {/* Ajout de l'attribut "name" */}
         </div>
         <div className="form-group">
           <label htmlFor="message">Message</label>
-          <textarea
-            id="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
+          <textarea id="message" name="message"></textarea> {/* Ajout de l'attribut "name" */}
         </div>
         <button type="submit">Envoyer</button>
       </form>
